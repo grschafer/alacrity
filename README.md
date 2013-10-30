5fcbe646789be6559d49b0204cd637e0c0209f8e
Greg Schafer <grschafer@gmail.com>
2013-10-30 17:44:06 -0500

    Refactor building_hp to use 1 list for all bldgs

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ git submodule init
 git submodule update
 install tarrasque and skadi to virtualenv
 
+run\_all will run the scripts specified inside it on all the .dem files in the directory/tree passed on command line
+
+to run an individual script on multiple files use:
+
+    find /path/to/demo/dir/ -type f | xargs -I {} python script_name.py {}
--- alacrity/building_hp.py
+++ alacrity/building_hp.py
@@ -34,26 +34,25 @@ import pdb
 # access by: building_hp[building_type][building_name][threshold value] = time threshold crossed
 # access by: building_hp[building_type][building_name] = {hp: threshold, time: crossed}
 
-# todo: profile this b/c it gets called so much
 def hp_pct(e):
     try:
-        return float(e.health) / e.max_health
+        return 100.0 * e.health / e.max_health
     except KeyError: # the entity is gone (died)
         return 0
 
 def extract_hp(replay):
-    tower_hp = {b.properties[(u'DT_BaseEntity', u'm_iName')]:{} for b in replay.buildings.towers}
-    rax_hp = {b.properties[(u'DT_BaseEntity', u'm_iName')]:{} for b in replay.buildings.barracks}
-    ancient_hp = {b.properties[(u'DT_BaseEntity', u'm_iName')]:{} for b in replay.buildings.ancients}
+    building_hp = {b.properties[(u'DT_BaseEntity', u'm_iName')]:{}
+                    for b in
+                    replay.buildings.towers + replay.buildings.barracks + replay.buildings.ancients}
 
-    game_towers = {b.properties[(u'DT_BaseEntity', u'm_iName')]:b for b in replay.buildings.towers}
-    game_rax = {b.properties[(u'DT_BaseEntity', u'm_iName')]:b for b in replay.buildings.barracks}
-    game_ancients = {b.properties[(u'DT_BaseEntity', u'm_iName')]:b for b in replay.buildings.ancients}
+    game_buildings = {b.properties[(u'DT_BaseEntity', u'm_iName')]:b
+                    for b in
+                    replay.buildings.towers + replay.buildings.barracks + replay.buildings.ancients}
 
     # stores health from prev iteration (to test for falling below a threshold)
-    tower_prevhp = {b.properties[(u'DT_BaseEntity', u'm_iName')]:hp_pct(b) for b in replay.buildings.towers}
-    rax_prevhp = {b.properties[(u'DT_BaseEntity', u'm_iName')]:hp_pct(b) for b in replay.buildings.barracks}
-    ancient_prevhp = {b.properties[(u'DT_BaseEntity', u'm_iName')]:hp_pct(b) for b in replay.buildings.ancients}
+    building_prevhp = {b.properties[(u'DT_BaseEntity', u'm_iName')]:hp_pct(b)
+                    for b in
+                    replay.buildings.towers + replay.buildings.barracks + replay.buildings.ancients}
 
     thresholds = range(90, -1, -10)
     cur_hp = None
@@ -62,56 +61,47 @@ def extract_hp(replay):
         if replay.info.pausing_team:
             continue
 
-        # TODO: all the following code is the same, REFACTOR
-        # towers
-        for name,b in game_towers.iteritems():
+        destroyed = []
+        for name,b in game_buildings.iteritems():
             cur_hp = hp_pct(b)
+
+            # mark building for removal from iteration list
+            if cur_hp == 0:
+                destroyed.append(name)
+
             # iterate all thresholds to account for building healing
             for thresh in thresholds:
                 # if it fell below threshold since last tick iteration, store the time
-                if cur_hp <= thresh < tower_prevhp[name]:
-                    tower_hp[name][thresh] = replay.info.game_time
-            tower_prevhp[name] = cur_hp
+                if cur_hp <= thresh < building_prevhp[name]:
+                    print name, thresh
+                    building_hp[name][thresh] = replay.info.game_time
+            building_prevhp[name] = cur_hp
 
-        # barracks
-        for name,b in game_rax.iteritems():
-            cur_hp = hp_pct(b)
-            for thresh in thresholds:
-                if cur_hp <= thresh < rax_prevhp[name]:
-                    rax_hp[name][thresh] = replay.info.game_time
-            rax_prevhp[name] = cur_hp
+        for d in destroyed:
+            print 'removing', d
+            del game_buildings[d]
 
-        # ancients
-        for name,b in game_ancients.iteritems():
-            cur_hp = hp_pct(b)
-            for thresh in thresholds:
-                if cur_hp <= thresh < ancient_prevhp[name]:
-                    ancient_hp[name][thresh] = replay.info.game_time
-            ancient_prevhp[name] = cur_hp
-
+    print building_hp
     # transform last dict to a list sorted by time
     #   this makes it so the javascript doesn't need to know the threshold values,
     #   it can just iterate and check for the most recent time
-    for name, threshtimes in tower_hp.iterkeys():
-        tower_hp[name] = sorted([{'hp': k, 'time': v} for k,v in threshtimes.iteritems()], key=lambda x: x['time'])
-    for name, threshtimes in rax_hp.iterkeys():
-        rax_hp[name] = sorted([{'hp': k, 'time': v} for k,v in threshtimes.iteritems()], key=lambda x: x['time'])
-    for name, threshtimes in ancient_hp.iterkeys():
-        ancient_hp[name] = sorted([{'hp': k, 'time': v} for k,v in threshtimes.iteritems()], key=lambda x: x['time'])
+    for name, threshtimes in building_hp.iteritems():
+        building_hp[name] = sorted([{'hp': k, 'time': v} for k,v in threshtimes.iteritems()], key=lambda x: x['time'])
 
-    return {'building_hp':{'towers':tower_hp, 'rax':rax_hp, 'ancients':ancient_hp}}
+    return {'building_hp': building_hp}
 
 def main():
     dem_file = sys.argv[1] # pass replay as cmd-line argument!
     replay = StreamBinding.from_file(dem_file, start_tick="pregame")
     match_id = replay.info.match_id
+    print 'match:', match_id
     #match = get_match_details(match_id)
     match = db.find_one({'match_id': match_id}) or {}
-    positions = extract_positions(replay)
-    print positions
+    hp = extract_hp(replay)
+    print hp
     #with open('pos.out', 'w') as f:
     #    f.write(str(positions))
-    match.update(positions)
+    match.update(hp)
     result = db.update({'match_id': match_id}, match, upsert=True)
 
 if __name__ == '__main__':
