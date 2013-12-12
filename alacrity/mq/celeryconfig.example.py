66f2670e5e19f70cdd937f950228fc2c3f980ac6
Greg Schafer <grschafer@gmail.com>
2013-12-12 12:08:33 -0700

    Update example celeryconfig file

--- alacrity/mq/celeryconfig.example.py
+++ alacrity/mq/celeryconfig.example.py
@@ -1,12 +1,21 @@
 from __future__ import absolute_import
+from datetime import timedelta
+from celery.schedules import crontab
 
 BROKER_URL = CELERY_RESULT_BACKEND = \
         'transport://userid:password@hostname:port/virtual_host'
 
 CELERY_IMPORTS = ['alacrity.mq.tasks']
 
-CELERY_TASK_SERIALIZER = 'json' # default: 'pickle'
-CELERY_RESULT_SERIALIZER = 'json' # default: 'pickle'
+#CELERY_TASK_SERIALIZER = 'json' # default: 'pickle'
+#CELERY_RESULT_SERIALIZER = 'json' # default: 'pickle'
+
+# remove results and result queues 1 hour after completion
+# shouldn't affect groups/chains even if worker is held up
+# because the results of one step are stored as arguments of the next
+#CELERY_TASK_RESULT_EXPIRES = 30 # time in seconds or timedelta
+
+#CELERY_IGNORE_RESULT = True # need results for .get() and chords?
 
 # long-running tasks, so we want them to spread evenly among workers
 CELERYD_PREFETCH_MULTIPLIER = 1 # default: 4
@@ -16,16 +25,14 @@ CELERY_ENABLE_UTC = True
 
 #CELERY_DEFAULT_RATE_LIMIT = '1/m'
 
-from datetime import timedelta
-from celery.schedules import crontab
 CELERYBEAT_SCHEDULE = {
         'find new matches': {
             'task': 'alacrity.mq.tasks.workflow',
-            'schedule': crontab(minute=35, hour=23),
+            'schedule': crontab(minute=16, hour=21),
         },
         'update leagues': {
             'task': 'alacrity.mq.tasks.update_leagues',
-            'schedule': crontab(minute=35, hour=23),
+            'schedule': crontab(minute=39, hour=20),
         },
 }
 
